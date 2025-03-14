34a58d35ca1c867622cc96bbc042b44014f7508c
VictorVaCa <a17vicvalcam@inspedralbes.cat>
2025-03-14 16:42:58 +0100

    Primer commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Marc_Cortes_Victor_Vallecillos_UF2_ACT2"
+include(":app")
+ 
